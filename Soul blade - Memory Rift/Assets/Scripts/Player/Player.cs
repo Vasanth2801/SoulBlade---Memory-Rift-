39f149c87127f8adbd9f3385478b2f4d778691d0
Vasanth2801 <vasanthravichandran28@gmail.com>
2026-07-10 15:14:31 +0530

    Player Variable jump and code refactor

--- Soul blade - Memory Rift/Assets/Scripts/Player/Player.cs
+++ Soul blade - Memory Rift/Assets/Scripts/Player/Player.cs
@@ -7,12 +7,32 @@ public class Player : MonoBehaviour
     [SerializeField] private float walkSpeed = 4f;
     [SerializeField] private int facingDirection = 1;
 
+    [Header("Jump Settings")]
+    [SerializeField] private float jumpForce = 10f;
+    [SerializeField] private float jumpCutMultiplier = 0.5f;
+    [SerializeField] private float normalGravity;
+    [SerializeField] private float jumpGravity;
+    [SerializeField] private float fallGravity;
+
+    [Header("Ground Check Settings")]
+    [SerializeField] private Transform groundCheck;
+    [SerializeField] private float groundCheckRadius = 0.3f;
+    [SerializeField] private LayerMask groundLayer;
+    [SerializeField] private bool isGrounded;
+ 
     [Header("Unity Components References")]
     [SerializeField] private Rigidbody2D rb;
     [SerializeField] private PlayerInput player;
 
     [Header("Inputs")]
     [SerializeField] private Vector2 moveInput;
+    [SerializeField] private bool jumpPressed;
+    [SerializeField] private bool jumpReleased;
+
+    private void Start()
+    {
+        rb.gravityScale = normalGravity;
+    }
 
     private void Update()
     {
@@ -21,13 +41,73 @@ public class Player : MonoBehaviour
 
     private void FixedUpdate()
     {
+        HandleMovement();
+        HandleJump();
+        CheckGrounded();
+        ApplyGravity();
+    }
+
+    void HandleMovement()
+    {
         float targetSpeed = moveInput.x * walkSpeed;
         rb.linearVelocity = new Vector2(targetSpeed, rb.linearVelocity.y);
+    }
+
+    void HandleJump()
+    {
+        if(jumpPressed && isGrounded)
+        {
+            rb.linearVelocity = new Vector2(rb.linearVelocity.x, jumpForce);
+            jumpPressed = false;
+            jumpReleased = false;
+        }
+        else if(jumpReleased)
+        {
+            if (rb.linearVelocity.y > 0.1f)
+            {
+                rb.linearVelocity = new Vector2(rb.linearVelocity.x, rb.linearVelocity.y * jumpCutMultiplier);
+            }
+            jumpReleased = false;
+        }
     }
 
     public void OnMove(InputValue value)
     {
         moveInput = value.Get<Vector2>();
+    }
+
+    public void OnJump(InputValue value)
+    {
+        if(value.isPressed && isGrounded)
+        {
+            jumpPressed = true;
+            jumpReleased = false;
+        }
+        else  // If we still going up
+        {
+           jumpReleased = true;
+        }
+    }
+
+    void CheckGrounded()
+    {
+        isGrounded = Physics2D.OverlapCircle(groundCheck.position, groundCheckRadius, groundLayer);
+    }
+
+    void ApplyGravity()
+    {
+        if(rb.linearVelocity.y < -0.1f)
+        {
+            rb.gravityScale = fallGravity;
+        }
+        else if(rb.linearVelocity.y > 0.1f)
+        {
+            rb.gravityScale = jumpGravity;
+        }
+        else
+        {
+            rb.gravityScale = normalGravity;
+        }
     }
 
     void Flip()
@@ -42,5 +122,11 @@ public class Player : MonoBehaviour
         }
 
         transform.localScale = new Vector3(facingDirection, 1f, 1f);
+    }
+
+    private void OnDrawGizmos()
+    {
+        Gizmos.color = Color.yellow;
+        Gizmos.DrawWireSphere(groundCheck.position, groundCheckRadius);
     }
 }
