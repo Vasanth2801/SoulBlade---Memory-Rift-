1e4ed7647f198f9266404d69715f76ac7f18c19b
Vasanth2801 <vasanthravichandran28@gmail.com>
2026-07-20 13:17:39 +0530

    Spark spell

--- Soul blade - Memory Rift/Assets/Scripts/Player/Core Components/Magic.cs
+++ Soul blade - Memory Rift/Assets/Scripts/Player/Core Components/Magic.cs
@@ -3,11 +3,20 @@ using UnityEngine;
 
 public class Magic : MonoBehaviour
 {
+    [Header("References")]
     [SerializeField] private  Player player;
+
+    [Header("Teleport Settings")]
     [SerializeField] private float spellRange;
     [SerializeField] private LayerMask  obstacleLayer;
     [SerializeField] private float spellCooldown;
     [SerializeField] private float playerRadius = 1.5f;
+
+    [Header("Spark Settings")]
+    [SerializeField] private GameObject sparkFX;
+    [SerializeField] private int damage;
+    [SerializeField] private float damageRadius;
+    [SerializeField] private LayerMask enemyLayer;
 
     public bool CanCast => Time.time >= nextCastTime;
     private float nextCastTime;
@@ -20,11 +29,16 @@ public class Magic : MonoBehaviour
 
     void CastSpell()
     {
-        Teleport();
+        //Teleport();
+        Spark();
     }
 
     private void Teleport()
     {
+        if(!CanCast)
+        {
+            return;
+        }
         Vector2 direction = new Vector2(player.facingDirection, 0);
         Vector2 targetPosition = (Vector2)player.transform.position + direction * spellRange;
 
@@ -44,6 +58,34 @@ public class Magic : MonoBehaviour
         }
 
         player.transform.position = targetPosition;
+        nextCastTime = Time.time + spellCooldown;
+    }
+
+    private void Spark()
+    {
+        if (!CanCast)
+        {
+            return;
+        }
+
+        Collider2D[] enemies = Physics2D.OverlapCircleAll(player.transform.position,damageRadius, enemyLayer);
+
+        foreach(Collider2D enemy in enemies)
+        {
+            Health health = enemy.GetComponent<Health>();
+
+            if(health != null )
+            {
+                health.ChangeHealth(-damage, enemy.transform.position);
+            }
+
+            if(sparkFX != null)
+            {
+                GameObject newFX = Instantiate(sparkFX, enemy.transform.position,Quaternion.identity);
+                Destroy(newFX,2);
+            }
+        }
+
         nextCastTime = Time.time + spellCooldown;
     }
 }
